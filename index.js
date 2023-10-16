fb87c246db192cf59857cb031bc6b97454fb996d
Maxwell <lubalecliff@gmail.com>
2023-10-16 16:05:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left);
+    dodger.style.left = `${left - 1}px`;
+  }
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left);
+    dodger.style.left = `${left + 1}px`;
+  }
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (event.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
